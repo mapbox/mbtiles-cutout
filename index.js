ff4a0f85b37fe2708afde87ac1c9ce14bc5c366a
Ian Ward <ianshward@gmail.com>
2016-06-03 00:35:43 -0400

    switch to pip approach

--- index.js
+++ index.js
@@ -2,55 +2,58 @@ var fs = require('fs');
 var path = require('path');
 var sqlite3 = require('sqlite3').verbose();
 var d3 = require('d3-queue');
-var cover = require('tile-cover');
+var inside = require('point-in-polygon');
+var SM = require('sphericalmercator');
+
+var merc = new SM({
+    size: 256
+});
 
 var cutout = module.exports;
+
 var blankTileData = new Buffer('H4sIAAAAAAAAA5My4WItTyxJLRKS4mAQYmJgkGBWEuD8z9jgJMXw37nBhYGhwYVfii2/ODc+M0WJSYFBo0GhghEAY2QLiTYAAAA=', 'base64');
 
 cutout.cut = function(options) {
 
-  var tilesToCut = [];
-  var tilesToBlank = [];
-  var database;
-  var q = d3.queue(1);
   var dbpath = path.join(process.cwd(), options.mbtilesFile);
   var toCut = options.shapeToCut ? JSON.parse(fs.readFileSync(options.shapeToCut)) : null;
   var toBlank = options.shapeToBlank ? JSON.parse(fs.readFileSync(options.shapeToBlank)) : null;
 
-  if (toCut) {
-    var cutLimits = {
-      min_zoom: options.cutMinZoom,
-      max_zoom: options.cutMaxZoom
-    };
-
-    tilesToCut = cover.tiles(toCut, cutLimits);
-  }
-
-  if (toBlank) {
-    var blankLimits = {
-      min_zoom: options.blankMinZoom,
-      max_zoom: options.blankMaxZoom
-    };
-
-    tilesToBlank = cover.tiles(toBlank, blankLimits);
-  }
-
   open(dbpath, function(err, db) {
+    if (err) throw err;
 
-    tilesToCut.forEach(function(coords) {
-      q.defer(deleteTile, db, coords);
-    });
-
-    tilesToBlank.forEach(function(coords) {
-      q.defer(blankTile, db, coords);
-    });
-
-    q.awaitAll(function(err) {
+    db.get('SELECT count(*) as cnt FROM map', function(err, res) {
       if (err) throw err;
-      // XXX close the DB
+      var rows = res.cnt;
+
+      var q = d3.queue(1);
+
+      for (var i = 0; i < rows; i += 10000) {
+        q.defer(function(done) {
+          db.all('SELECT * FROM map WHERE ROWID >= ? AND ROWID < ?', i, i+10000, function(err, res) {
+            if (err) return done(err);
+            res.forEach(function(row) {
+              var ll = merc.ll([row.tile_row, row.tile_column], row.zoom_level);
+              var cut;
+              var blank;
+              if (toCut) cut = inside(ll, toCut);
+              if (toBlank) blank = inside(ll, toBlank);
+              alterTile(db, row.tile_id, cut, blank, function(err) {
+                done(err);
+              });
+            });
+          });
+        });
+      }
+
+      q.awaitAll(function(err) {
+        console.log(err);
+      });
+
     });
 
   });
+
 
 };
 
@@ -61,44 +64,31 @@ function open(database, cb) {
   });
 }
 
-function deleteTile(database, coords, cb) {
+function alterTile(db, tile_id, cut, blank, cb) {
+    var q = d3.queue();
 
-  var column = coords[1];
-  var row = coords[0];
-  var zoom = coords[2];
-
-  database.get('SELECT tile_id FROM map WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?',
-    zoom, column, row, function(err, res) {
-    //4, 15, 15, function(err, res) {
-    if (err) return cb(err);
-    if (!res) return cb(null);
-    var tile_id = res.tile_id;
-    database.run('DELETE FROM images WHERE tile_id = ?', tile_id, function(err) {
-      if (err) return cb(err);
-      database.run('DELETE FROM map WHERE tile_id = ?', tile_id, function(err) {
-        if (err) return cb(err);
-        cb();
+    if (cut) {
+      q.defer(function(done) {
+        db.run('DELETE FROM images WHERE tile_id = ?', tile_id, function(err) {
+          if (err) return done(err);
+          db.run('DELETE FROM map WHERE tile_id = ?', tile_id, function(err) {
+            if (err) return done(err);
+            cb();
+          });
+        });
       });
+    }
+
+    // Don't allow blanking AND cutting
+    if (blank && !cut) {
+      q.defer(function(done) {
+        db.run('UPDATE images SET tile_data = ? WHERE tile_id = ?', blankTileData, tile_id, function(err) {
+          done(err);
+        });
+      });
+    }
+
+    q.awaitAll(function(err) {
+      cb(err);
     });
-  });
-
-}
-
-function blankTile(database, coords, cb) {
-
-  var column = coords[1];
-  var row = coords[0];
-  var zoom = coords[2];
-
-  database.get('SELECT tile_id FROM map WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?',
-    zoom, column, row, function(err, res) {
-    if (err) return cb(err);
-    if (!res) return cb(null);
-    var tile_id = res.tile_id;
-    database.run('UPDATE images SET tile_data = ? WHERE tile_id = ?',
-      blankTileData, tile_id, function(err) {
-        cb(err);
-      });
-  });
-
 }
